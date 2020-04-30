bffdfc7a3fb7274af86ab9d0f1a1b1d1b71c7547
afeiii777 <798561721@qq.com>
2020-04-30 23:55:03 +0800

    main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+//#include"ExpressTable.h"
+//#include"ExpressCell.h"
+//#include"Compare.h"
+//#include"Express.h"
 using namespace std;
 int main() {
-	cout << "冲冲冲";
+	while (1) {
+		int op;
+		cout << "主菜单"<<endl;
+		cout << "1.快递员" << endl << "2.取件人" << endl << "3.管理员" << endl<<"0.结束"<<endl;
+		cin >> op;
+		if (op == 0) {
+			system("cls");
+			//save();
+			return 0;
+		}
+		else if (op == 1) {
+			system("cls");
+			//Expressman();
+			//save();
+		}
+		else if (op == 2) {
+			system("cls");
+			//Receiver();
+			//save();
+		}
+		else if (op == 3) {
+			system("cls");
+			//Administrator();
+			//save();
+		}
+		else {
+			system("cls");
+			cout << "非法输入"<<endl;
+			continue;
+		}
+	}
 	return 0;
 }
