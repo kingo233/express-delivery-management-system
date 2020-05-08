d7ddb59cfebed969240b70037782b0c756856413
afeiii777 <798561721@qq.com>
2020-05-08 18:45:37 +0800

    比较函数

--- Compare.cpp
+++ Compare.cpp
@@ -1,1 +1,44 @@
 #include "Compare.h"
+#include"Express.h"
+#include<iostream>
+using namespace std;
+bool Compare::CompanyNameAscendingCmp(ExpressCell a, ExpressCell b) 
+{
+	return a.CompanyName < b.CompanyName;
+}
+bool Compare::OwnerNameAscendingCmp(ExpressCell a, ExpressCell b) 
+{
+	return a.OwnerName < b.OwnerName;
+}
+bool Compare::PhoneNumerAscendingCmp(ExpressCell a, ExpressCell b) 
+{
+	return a.PhoneNumber < b.PhoneNumber;
+}
+bool Compare::IndexAscendingCmp(ExpressCell a, ExpressCell b) 
+{
+	return a.Index < b.Index;
+}
+bool Compare::TimeAscendingCmp(ExpressCell a, ExpressCell b) 
+{
+	return a.Time < b.Timer;
+}
+bool Compare::CompanyNameAscendingCmp(ExpressCell a, ExpressCell b)
+{
+	return a.CompanyName > b.CompanyName;
+}
+bool Compare::OwnerNameAscendingCmp(ExpressCell a, ExpressCell b)
+{
+	return a.OwnerName > b.OwnerName;
+}
+bool Compare::PhoneNumerAscendingCmp(ExpressCell a, ExpressCell b)
+{
+	return a.PhoneNumber > b.PhoneNumber;
+}
+bool Compare::IndexAscendingCmp(ExpressCell a, ExpressCell b)
+{
+	return a.Index > b.Index;
+}
+bool Compare::TimeAscendingCmp(ExpressCell a, ExpressCell b)
+{
+	return a.Time > b.Timer;
+}
