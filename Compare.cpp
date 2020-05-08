31ed1416bffe41cc15e4b923ddd308c838841bd7
afeiii777 <798561721@qq.com>
2020-05-08 20:21:22 +0800

    改改改冲冲冲

--- Compare.cpp
+++ Compare.cpp
@@ -21,23 +21,23 @@ bool Compare::TimeAscendingCmp(ExpressCell a, ExpressCell b)
 {
 	return a.Timer < b.Timer;
 }
-bool Compare::CompanyNameAscendingCmp(ExpressCell a, ExpressCell b)
+bool Compare::CompanyNameDescendingCmp(ExpressCell a, ExpressCell b)
 {
 	return a.ExpressPointer->CompanyName > b.ExpressPointer->CompanyName;
 }
-bool Compare::OwnerNameAscendingCmp(ExpressCell a, ExpressCell b)
+bool Compare::OwnerNameDescendingCmp(ExpressCell a, ExpressCell b)
 {
 	return a.ExpressPointer->OwnerName > b.ExpressPointer->OwnerName;
 }
-bool Compare::PhoneNumerAscendingCmp(ExpressCell a, ExpressCell b)
+bool Compare::PhoneNumerDescendingCmp(ExpressCell a, ExpressCell b)
 {
 	return a.ExpressPointer->PhoneNumber > b.ExpressPointer->PhoneNumber;
 }
-bool Compare::IndexAscendingCmp(ExpressCell a, ExpressCell b)
+bool Compare::IndexDescendingCmp(ExpressCell a, ExpressCell b)
 {
 	return a.Index >  b.Index;
 }
-bool Compare::TimeAscendingCmp(ExpressCell a, ExpressCell b)
+bool Compare::TimeDescendingCmp(ExpressCell a, ExpressCell b)
 {
 	return a.Time > b.Timer;
 }
