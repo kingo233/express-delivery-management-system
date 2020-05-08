2eb7b6e213952c2c99b18ab4b476608121b67b85
kingo233 <kingo233@outlook.com>
2020-05-08 17:52:18 +0800

    修改了快递柜的构造函数

--- ExpressTable.cpp
+++ ExpressTable.cpp
@@ -6,18 +6,38 @@ ExpressTable::ExpressTable()
 {
 	memset(MyCells, 0, sizeof(MyCells));
 	FILE* FilePointer;
-	const char FileName[20] = "Cells";
-	if (fopen_s(&FilePointer,FileName,"rb") == 0) 
+	const char FileName[20] = "Cells.dat";
+	if (fopen_s(&FilePointer,FileName,"rb") == 0 && FilePointer != NULL ) 
 	{
-		
-
+		//打开文件成功
+		for (int i = 0; i < maxn; i++) 
+		{
+			fread(MyCells[i], sizeof(ExpressCell), 1, FilePointer);
+		}
 	}
-	else {
+	else
+	{
+		//文件不存在
+		//创建数据文件
+		if (fopen_s(&FilePointer, FileName, "wb") == 0);
+		else 
+		{
+			cout << "创建快递柜数据文件失败！" << endl;
+			return;
+		}
 
+		//为MyCells初始化
+		for (int i = 0; i < maxn; i++)
+		{
+			MyCells[i] = new ExpressCell;
+			MyCells[i]->Index = i;
+		}
 	}
+
 }
 
 ExpressTable::~ExpressTable()
 {
+	
 }
 
