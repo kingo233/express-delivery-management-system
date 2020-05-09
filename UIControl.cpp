f739a3c1c97c2dadc40243ce115495aa88965c2e
kingo233 <kingo233@outlook.com>
2020-05-09 15:37:43 +0800

    简化代码

--- UIControl.cpp
+++ UIControl.cpp
@@ -1,8 +1,10 @@
 #include "UIControl.h"
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 void UIControl::ShowHome() 
 {
+	system("cls");
 	cout << "欢迎使用快递柜管理系统！" << endl;
 	cout << "主菜单" << endl;
 	cout << "1.快递员" << endl << "2.取件人" << endl << "3.管理员" << endl << "4.UI界面设置" << endl << "0.结束" << endl;
@@ -21,5 +23,52 @@ void UIControl::ShowAdminstrator()
 }
 void UIControl::ShowUISetting() 
 {
-
+	while (1) {
+		string cop;
+		cout << "请选择字体背景颜色设置：" << endl << "1.白色背景红色字体" << endl << "2.黑色背景白色字体" << endl
+			<< "3.紫色背景黄色字体（猛男专用）" << endl << "4.绿色背景黑色字体（护眼模式）" << endl;
+		cin >> cop;
+		if (cop == "1")
+		{
+			system("color 74");
+			system("cls");
+			cout << "设置成功！";
+			Sleep(1000);
+			system("cls");
+			break;
+		}
+		else if (cop == "2")
+		{
+			system("color 07");
+			system("cls");
+			cout << "设置成功！";
+			Sleep(1000);
+			system("cls");
+			break;
+		}
+		else if (cop == "3")
+		{
+			system("color 56");
+			system("cls");
+			cout << "设置成功！";
+			Sleep(1000);
+			system("cls");
+			break;
+		}
+		else if (cop == "4")
+		{
+			system("color 20");
+			system("cls");
+			cout << "设置成功！";
+			Sleep(1000);
+			system("cls");
+			break;
+		}
+		else {
+			system("cls");
+			cout << "请输入正确的操作数！";
+			Sleep(1000);
+			system("cls");
+		}
+	}
 }
--- main.cpp
+++ main.cpp
@@ -13,85 +13,36 @@ int main()
 		UIControl::ShowHome();
 		string op;
 		cin >> op;
-
 		system("cls");
-		if (op == "0") 
+		if (op == "0")
 		{
 			//save();
 			return 0;
 		}
-		else if (op == "1") 
+		else if (op == "1")
 		{
 			cout << "快递员";
 			//Expressman();
 			//save();
 		}
-		else if (op == "2") 
+		else if (op == "2")
 		{
 			cout << "取件人";
 			//Receiver();
 			//save();
 		}
-		else if (op == "3") 
+		else if (op == "3")
 		{
 			cout << "管理员";
 			//Administrator();
 			//save();
 		}
-		else if (op == "4") 
+		else if (op == "4")
 		{
-			string cop;//选择界面颜色
-			while (1) {
-				cout << "请选择字体背景颜色设置：" << endl << "1.白色背景红色字体" << endl << "2.黑色背景白色字体" << endl << "3.紫色背景黄色字体（猛男专用）" << endl;
-				cout << "4.绿色背景黑色字体（护眼模式）" << endl;
-				cin >> cop;
-				if (cop == "1")
-				{
-					system("color 74");
-					system("cls");
-					cout << "设置成功！";
-					Sleep(1000);
-					system("cls");
-					break;
-				}
-				else if (cop == "2")
-				{
-					system("color 07");
-					system("cls");
-					cout << "设置成功！";
-					Sleep(1000);
-					system("cls");
-					break;
-				}
-				else if (cop == "3")
-				{
-					system("color 56");
-					system("cls");
-					cout << "设置成功！";
-					Sleep(1000);
-					system("cls");
-					break;
-				}
-				else if (cop == "4")
-				{
-					system("color 20");
-					system("cls");
-					cout << "设置成功！";
-					Sleep(1000);
-					system("cls");
-					break;
-				}
-				else {
-					system("cls");
-					cout << "请输入正确的操作数！";
-					Sleep(1000);
-					system("cls");
-				}
-			}
+			
 		}
 		else 
 		{
-
 			cout << "请输入正确的操作数！" << endl;
 			Sleep(1000);//暂停1秒
 			system("cls");
