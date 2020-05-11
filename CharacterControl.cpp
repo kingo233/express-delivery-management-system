a1059cd2bc1db7751c12c8230a4df711c53fc470
佳豪 单 <2351278764@qq.com>
2020-05-11 10:45:00 +0800

    Expressman函数 和 showExpressman函数的实现

--- CharacterControl.cpp
+++ CharacterControl.cpp
@@ -1,12 +1,61 @@
 #include "CharacterControl.h"
+#include "ExpressTable.h"
 #include "UIControl.h"
 #include <Windows.h>
 #include <iostream>
 #include<fstream>
 using namespace std;
-void CharacterControl::ExpressMan() 
+void CharacterControl::ExpressMan()
 {
+	while (true) {
+		UIControl::ShowExpressMan();
+		ExpressTable Table;
+		Express e;
+		string ex;
+		cin >> ex;
+		int i;
+		if (ex == "1")
+		{
+			for (i = 0; i <= 500; i++)
+			{
+				if (Table.MyCells[i] == 0)
+				{
+					Table.MyCells[i] = new ExpressCell();
+					if (Table.PlaceExpress(e))
+					{
+						struct tm* Time;
+						Table.MyCells[i]->Index = i;
+						Table.MyCells[i]->Timer = time(NULL);
+						Time = localtime(&Table.MyCells[i]->Timer);
+						Table.MyCells[i]->StrTime = asctime(Time);
+						break;
+					}
+					else
+					{
+						cout << "快递放入失败" << endl;
+						break;
+					}
+				}
 
+			}
+			if (i > 500)
+			{
+				cout << "快递格已满,无法放入" << endl;
+			}
+		}
+		else if (ex == "0")
+		{
+			system("cls");
+			return;
+		}
+		else
+		{
+			system("cls");
+			cout << "请输入正确的操作数！";
+			Sleep(1000);
+			system("cls");
+		}
+	}
 }
 void CharacterControl::Receiver() 
 {
