393fa0a2a00d2c8a996c1d5c362f6d92a4ecae9d
佳豪 单 <2351278764@qq.com>
2020-05-16 21:43:50 +0800

    receiver 函数修改

--- ExpressTable.h
+++ ExpressTable.h
@@ -13,15 +13,12 @@ public:
 	bool PlaceExpress(int Postion);
 
 	//取出快递，成功返回true
-	bool TakeExpress(int Postion);
+	bool TakeExpress(int Postion, char* phonenumber, char* ownername);
 
 	//查询快递柜是否已满，满了返回-1，没满返回第一个空的快递格的位置
 	int IsFull();
 
-	//利用成员函数访问私有成员
-	char* returnOwnerName(int index);
-	char* returnPhoneNumber(int index);
-	int returnTimer(int index);
+
 
 private:
 	const int MAXN = 500;
