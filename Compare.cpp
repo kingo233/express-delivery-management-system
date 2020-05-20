ed07ca6d4f8b7f84a56b6dacf0c97e7ea21855aa
afeiii777 <798561721@qq.com>
2020-05-20 19:50:42 +0800

    比较函数更改

--- Compare.cpp
+++ Compare.cpp
@@ -36,35 +36,35 @@ bool Compare::TimeAscendingCmp(ExpressCell* a, ExpressCell* b)
 }
 bool Compare::CompanyNameDescendingCmp(ExpressCell* a, ExpressCell* b)
 {
-	if (b->Timer == 0) return false;
-	else if (a->Timer == 0) return true;
+	if (a->Timer == 0) return false;
+	else if (b->Timer == 0) return true;
 	else if (strcmp(a->Express.CompanyName, b->Express.CompanyName) > 0) return true;
 	else return false;
 }
 bool Compare::OwnerNameDescendingCmp(ExpressCell* a, ExpressCell* b)
 {
-	if (b->Timer == 0) return false;
-	else if (a->Timer == 0) return true;
+	if (a->Timer == 0) return false;
+	else if (b->Timer == 0) return true;
 	else if (strcmp(a->Express.OwnerName, b->Express.OwnerName) > 0) return true;
 	else return false;
 }
 bool Compare::PhoneNumberDescendingCmp(ExpressCell* a, ExpressCell* b)
 {
-	if (b->Timer == 0) return false;
-	else if (a->Timer == 0) return true;
+	if (a->Timer == 0) return false;
+	else if (b->Timer == 0) return true;
 	else if (strcmp(a->Express.PhoneNumber, b->Express.PhoneNumber) > 0) return true;
 	else return false;
 }
 bool Compare::IndexDescendingCmp(ExpressCell* a, ExpressCell* b)
 {
-	if (b->Timer == 0) return false;
-	else if (a->Timer == 0) return true;
+	if (a->Timer == 0) return false;
+	else if (b->Timer == 0) return true;
 	return a->Index >  b->Index;
 }
 bool Compare::TimeDescendingCmp(ExpressCell* a, ExpressCell* b)
 {
-	if (b->Timer == 0) return false;
-	else if (a->Timer == 0) return true;
+	if (a->Timer == 0) return false;
+	else if (b->Timer == 0) return true;
 	return a->Timer > b->Timer;
 }
 bool Compare::ResetCmp(ExpressCell* a, ExpressCell* b) 
