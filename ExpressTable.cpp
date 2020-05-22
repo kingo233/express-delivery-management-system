c96b85002c82a8e0d2e7849b80752c180e6eee0b
afeiii777 <798561721@qq.com>
2020-05-22 10:49:00 +0800

    Revert "123"
    This reverts commit 8da1d0f920459bcf478c11a54d674bcf68b3c1a6.

--- ExpressTable.cpp
+++ ExpressTable.cpp
@@ -170,6 +170,7 @@ bool ExpressTable::TakeExpress(int Postion, char* phonenumber, char* ownername)
 	if (MyCells[Postion]->Timer == 0)
 	{
 		//timer为0就是没有快递
+		cout << "该位置没有快递！" << endl;
 		return false;
 	}
 	else
