dc5e245510047caddf11b5d06a771bec3e5c36bf
kingo233 <kingo233@outlook.com>
2020-05-25 21:32:00 +0800

    增加注释

--- ExpressCell.cpp
+++ ExpressCell.cpp
@@ -9,5 +9,6 @@ ExpressCell::~ExpressCell() {
 
 void ExpressCell::PrintInfo()
 {
+	//格式化打印信息
 	printf("%-4s  %-11s  %-10s%s\n", Express.OwnerName, Express.PhoneNumber, Express.CompanyName, StrTime);
 }
