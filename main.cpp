22422a70e4cf074f0cbdbe3b692173e82116b251
kingo233 <kingo233@outlook.com>
2020-05-27 21:14:56 +0800

    优化代码

--- main.cpp
+++ main.cpp
@@ -18,7 +18,8 @@ int main()
 		cout << "欢迎使用快递柜管理系统！" << endl;
 		cout << "主菜单" << endl;
 		cout << "1.快递员" << endl << "2.取件人" << endl << "3.管理员" << endl << "4.UI界面设置" << endl << "0.结束" << endl;
-
+		cout << "如果您对本系统熟悉，可以使用操作数加空格加下一个界面的操作数来加速操作" << endl;
+		
 		string op;
 		cin >> op;
 
